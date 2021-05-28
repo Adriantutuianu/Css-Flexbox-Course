7a877b3919b31df93bd74bf918219123d86b2708
Adrian <adriantutuianu01@gmail.com>
2021-05-29 00:02:59 +0100

    setup Gulp

--- autoprefixer/gulpfile.js
+++ autoprefixer/gulpfile.js
@@ -1,2 +1,6 @@
 var gulp = require("gulp");
 var autoprefixer = require("gulp-autoprefixer");
+
+gulp.task("styles", function () {
+  gulp.src("css/styles.css").pipe(autoprefixer()).pipe(gulp.dest("build"));
+});
